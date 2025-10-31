8b6d2643daa535db3135a2a98af59b6dc4cdb1f5
TEA-Katya <ka29tya@gmail.com>
2025-11-01 02:05:15 +0300

    Create Практ7 (15.1).py
    1 Найти все простые натуральные числа, не превосходящие n, двоичная запись
    которых представляет собой палиндром, т. е. читается одинаково слева
    направо и справа налево.

---
+++ Практ7 (15.1).py
@@ -0,0 +1,20 @@
+def is_prime(n):
+    #Проверяет, является ли число простым.
+    if n < 2: return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0: return False
+    return True
+
+def is_binary_palindrome(n):
+    #Проверяем, является ли двоичная запись числа палиндромом.
+    binary = bin(n)[2:] # Получаем двоичную запись, обрезая префикс '0b'
+    return binary == binary[::-1]
+
+def find_binary_numbers(n_limit):
+    #Находит все простые числа, не превосходящие n_limit, чья двоичная запись - палиндром.
+    return [n for n in range(2, n_limit + 1) if is_prime(n) and is_binary_palindrome(n)]
+
+
+n = int(input("Задайте верхнюю границу: " ))
+result = find_binary_numbers(n)
+print(f"Простые числа до {n}, двоичная запись которых - палиндром: {res
