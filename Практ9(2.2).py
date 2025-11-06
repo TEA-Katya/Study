45874a3f6564209fa380b92e9f1807da8c3913e2
TEA-Katya <ka29tya@gmail.com>
2025-11-06 15:02:50 +0300

    Update Практ9(2.2).py

--- Практ9(2.2).py
+++ Практ9(2.2).py
@@ -6,20 +6,17 @@ def find_second_max(current_max, current_second_max):
         return find_second_max(current_max, current_second_max)
 
     if num == 0:
-        # Если встретили 0, значит, ввод закончен. Возвращаем второе по величине число.
+        #Встречаем 0 - ввод закончен. Возвращаем второе по величине число.
         return current_second_max
     else:
         if num > current_max:
-            # Если новое число больше текущего максимума, старый максимум становится новым вторым максимумом, а новое число - новым максимумом.
             return find_second_max(num, current_max)
         elif num > current_second_max:
-            # Если новое число не больше максимума, но больше второго максимума, оно становится новым вторым максимумом.
             return find_second_max(current_max, num)
         else:
-            # В остальных случаях (число меньше или равно второму максимуму), значения не меняются, просто продолжаем с тем же.
             return find_second_max(current_max, current_second_max)
 
 #Запускаем рекурсию.(Если первое введенное число будет 5, то max станет 5, а second_max останется 0)
 result = find_second_max(0, 0)
 
-print(f"Второе по величине число: {result}")
+print(f"Второе по величине число: ", result)
